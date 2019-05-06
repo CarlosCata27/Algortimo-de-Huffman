dee3245e77a5b5212897005189abac5e31094bee
Carlos Catarino <43734195+CarlosKta27@users.noreply.github.com>
2019-05-05 23:13:52 -0500

    Build 1.0.0

--- main.c
+++ main.c
@@ -2,35 +2,12 @@
 #include <stdlib.h>
 #include "funciones.h"
 
-
-
-
 int main() {
-    Nodo *lista = NULL, *arbol = NULL;
-    Binario *listacod = NULL;
-
-    char abc[29] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-                    'u', 'v', 'w', 'x', 'y', 'z', '+', '.', ','};
-    int count[29] = {0}, m;
-    char *nombre = "E:\\Escuela\\Estructura_de_datos\\Practica 4\\original.txt", *salida = "E:\\Escuela\\Estructura_de_datos\\Practica 4\\salida.txt";
-    char *salida2 = "E:\\Escuela\\Estructura_de_datos\\Practica 4\\codificado.txt", frase[100], *codbinario = "";
-    char *salida3 = "E:\\Escuela\\Estructura_de_datos\\Practica 4\\binarios.txt";
-    FILE *archivo = fopen(nombre, "rt"), *out2 = fopen(salida2, "w"), *out = fopen(salida, "w"), *out3 = fopen(salida3,"w"),*ropen = fopen(salida3,"rt");
-
-    if (archivo == NULL || !out || !out2) {
-        puts("Este archivo no existe");
-        exit(-1);
-    }
-
-    fgets(frase, 100, archivo);
-    textoconvertido(frase);
-    contador(frase, count);
-    for (int j = 0; j < 29; j++) {
-        if (count[j] != 0) {
-            lista = altainicio(count[j], lista, abc[j]);
-        }
-    }
-    lista = ordenar_seleccion(lista);
+    int m;
+    char *nombre = "E:\\Escuela\\Estructura_de_datos\\Practica 4\\frase.txt";
+    char *salida = "E:\\Escuela\\Estructura_de_datos\\Practica 4\\arbol_codificado.txt";
+    char *salida2 = "E:\\Escuela\\Estructura_de_datos\\Practica 4\\frase_codificada.txt";
+    char *salida3 = "E:\\Escuela\\Estructura_de_datos\\Practica 4\\frase_binaria.txt";
 
     puts("ALGORITMO DE HUFFMAN\n"
          "1 >> Codificar\n"
@@ -39,7 +16,28 @@ int main() {
     scanf_s("%d", &m);
     switch (m) {
         case 1:
-            Nodo *aux;
+            char abc[29] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                            's', 't',
+                            'u', 'v', 'w', 'x', 'y', 'z', '+', '.', ','}, frase[100], *codbinario = "";
+            int count[29] = {0};
+            FILE *archivo = fopen(nombre, "rt"), *out2 = fopen(salida2, "w"), *out = fopen(salida, "w"), *out3 = fopen(
+                    salida3, "w"), *ropen = fopen(salida3, "rt");
+
+            if (archivo == NULL || !out || !out2) {
+                puts("Este archivo no existe");
+                exit(-1);
+            }
+            Nodo *lista = NULL, *arbol = NULL, *aux;
+            Binario *listacod = NULL;
+            fgets(frase, 100, archivo);
+            textoconvertido(frase);
+            contador(frase, count);
+            for (int j = 0; j < 29; j++) {
+                if (count[j] != 0) {
+                    lista = altainicio(count[j], lista, abc[j]);
+                }
+            }
+            lista = ordenar_seleccion(lista);
             arbol = lista;
             while (arbol && arbol->sig) {
                 int nv = arbol->dato + arbol->sig->dato;
@@ -50,19 +48,18 @@ int main() {
                 Buildtree(&arbol, aux);
             }
             preOrdenimpresion(arbol, out);
-            Buildbinary(arbol, 0, 0, &listacod,0);
+            Buildbinary(arbol, 0, 0, &listacod, 0);
             mostrarbin(listacod);
-            obtbin(listacod,frase,out3);
+            obtbin(listacod, frase, out3);
             fclose(out3);
 
-            fgets(codbinario,500,ropen);
+            fgets(codbinario, 500, ropen);
             fclose(ropen);
-            while (strlen(codbinario)%8!=0)
-            {
-                sprintf(codbinario, "%s0",codbinario);
+            while (strlen(codbinario) % 8 != 0) {
+                sprintf(codbinario, "%s0", codbinario);
             }
-            int *caracteres = (int *)malloc((strlen(codbinario)/8)* sizeof(int)),rec=0;
-            for (int i = 0; i < (strlen(codbinario)/8); i++) {
+            int *caracteres = (int *) malloc((strlen(codbinario) / 8) * sizeof(int)), rec = 0;
+            for (int i = 0; i < (strlen(codbinario) / 8); i++) {
                 char byte[8];
                 for (int j = 0; j < 8; j++) {
                     byte[j] = codbinario[rec];
@@ -70,9 +67,9 @@ int main() {
                 }
                 caracteres[i] = toDecimal(atoi(byte));
             }
-            for (int k = 0; k <(strlen(codbinario)/8); ++k) {
-                fprintf(out2,"%c",caracteres[k]);
-                printf("%d | %c\n",caracteres[k],caracteres[k]);
+            for (int k = 0; k < (strlen(codbinario) / 8); ++k) {
+                fprintf(out2, "%c", caracteres[k]);
+                printf("%d | %c\n", caracteres[k], caracteres[k]);
             }
             fclose(out);
             fclose(out2);
